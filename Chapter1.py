45b45ff8b127abb7f173fa8f76d5b0cf52419290
nahiefhspc <t.al.khackheist@gmail.com>
2025-04-14 06:12:40 +0530

    Create Chapter1.py

---
+++ Chapter1.py
@@ -0,0 +1,28 @@
+#Variables - Strings,Inegters,Float,Boolen
+
+#These are Strings - with commas
+first_name = "Bro"
+
+
+print(f"anything {first_name}")
+
+#These are Integers - not with commas
+age = 25
+
+print(f"your are {age}")
+
+#Float
+price = 10.99
+
+print(f"this is {price}dollar")
+
+#Boolean - only two option True/False
+is_student = False
+
+##print(f"Are you op {is_student}")
+if is_student :
+    print("you are loser")
+else :
+    print("you are not student")
+
+
