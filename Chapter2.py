feb9bf78ee73278204953cc288c74fc69904a21c
nahiefhspc <t.al.khackheist@gmail.com>
2025-04-14 06:13:21 +0530

    Create Chapter2.py

---
+++ Chapter2.py
@@ -0,0 +1,22 @@
+# Typecasting = the process of converting a variable from one data type to another
+#     str(), int(), float(), bool()
+ 
+name = "Bro code"
+age = 25
+gpa = 3.3
+is_student = True
+
+print(type(is_student))
+
+gpa = int(gpa)
+
+print(gpa)
+
+age = str(age)
+
+age += "1"
+print(age)
+
+name = bool(name)
+
+print(name)
