4075530d3ff03fea1d5a142c15592ff2e260d832
nahiefhspc <t.al.khackheist@gmail.com>
2025-04-14 06:13:49 +0530

    Create Chapter3.py

---
+++ Chapter3.py
@@ -0,0 +1,10 @@
+# input() = A function that promots the user to enter data Returns the entered data as a string
+
+name = input("What is your name?:")
+age = int(input("How old are you"))
+
+#age = int(age)
+age = age + 1
+
+print(f"Hello {name}")
+print(f"Age {age}")
