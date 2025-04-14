2aad0aacbcce769072253e25c7a6440958864a1b
nahiefhspc <t.al.khackheist@gmail.com>
2025-04-14 06:14:46 +0530

    Update Chapter3.py

--- Chapter3.py
+++ Chapter3.py
@@ -8,3 +8,32 @@ age = age + 1
 
 print(f"Hello {name}")
 print(f"Age {age}")
+
+
+
+# Exercise 1 Recatangle Area Calc
+
+length = int(input("Enter the length: "))
+width = int(input("Enter the Width: "))
+area = length * width
+#Try - Float,Int
+
+print(area)
+
+
+
+
+
+# Exercise 2 Shopping Cart Program
+
+item = input("What item would you like to buy? ")
+price = float(input(" What is Price ? "))
+quantity = int(input(" How many would you like?: "))
+total = price * quantity
+
+print(f" You Have bought {quantity} × {item}/s")
+print(f"Your total is : {total}")
+
+
+
+
