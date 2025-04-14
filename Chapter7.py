3144c2e811c8625bacb51c6578d84c62ccfea688
nahiefhspc <t.al.khackheist@gmail.com>
2025-04-14 12:59:10 +0530

    Update Chapter7.py

--- Chapter7.py
+++ Chapter7.py
@@ -2,3 +2,32 @@
 #                     or = at least one condition must be True
 #                     and = both conditions must be True
 #                     not = inverts the condition (not False, not True)
+
+#or
+temp = 25
+is_raining = False
+
+if temp > 35 or temp < 0 or is_raining:
+  print("The outdoor event is cancelled")
+
+else:
+  print("The outdoor event is still scheduled")
+
+
+#and
+temp = 25
+is_sunny = True
+
+if temp >= 28 and is_sunny:
+  print("It is Hot Outside")
+  print("It is sunny 🥲")
+
+elif temp <= 0 and is_sunny:
+  print("It is Cold Outside")
+  print("It is sunny 🥲")
+
+elif 28 > temp > 0 and is_sunny:
+  print("It is Warm Outside")
+  print("It is sunny 🥲")
+
+
