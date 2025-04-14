fb5d9741df1ab5235562b3ca254fb10f457f3a42
nahiefhspc <t.al.khackheist@gmail.com>
2025-04-14 06:16:10 +0530

    Create Chapter4.py

---
+++ Chapter4.py
@@ -0,0 +1,44 @@
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input(" Enter(person,place,thing) ")
+adjective2 = input(" Enter (description): ")
+verb1 = input(" Enter a verb ending with 'ing' ")
+adjective3 = input(" Enter (description): ")
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit,I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
+
+
+
+#Exercies 
+friends = 27
+
+# friends = friends + 1
+# friends += 1
+# friends = friends -2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends/2
+# friends /= 2
+# friends = friends ** 2
+# friends **= 2
+# remainder = friends % 3
+print(remainder)
+
+
+#Exercise
+x = 3.14
+y = -4
+z = 5
+
+#result = round(x)
+#result = abs(y)
+# result = pow(4, 3)
+result = max(x, y, z)
+result = min(x, y, z)
+print(result)
